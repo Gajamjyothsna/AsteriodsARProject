7a1bdf66cb116125ef2a5edfbdd5ada10ef2feb4
GAJAM JYOTHSNA <96329766+Gajamjyothsna@users.noreply.github.com>
2024-06-21 18:46:52 +0530

    Created SetInitialRotationTowardsTarget script

--- Assets/Scripts/SetInitialRotationTowardsTarget.cs
+++ Assets/Scripts/SetInitialRotationTowardsTarget.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class SetInitialRotationTowardsTarget : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private string targetObjectName;
+    [SerializeField] private Vector3 randomOffset;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        //Find the target with name of the object
+        GameObject targetObject = GameObject.Find(targetObjectName);
+
+        //If target object is not found, then its nothing
+        if (targetObject == null) return;
+
+        //Get the target's position to get the direction
+        Vector3 direction = targetObject.transform.position - this.transform.position;
+
+        //Randomize based on the offset and add to the direction
+        direction += new Vector3(Random.Range(randomOffset.x, -randomOffset.x),
+            Random.Range(randomOffset.y, -randomOffset.y), Random.Range(randomOffset.z, -randomOffset.z));
+
+        //Rotating the object towards the target object
+        transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
     }
 }
